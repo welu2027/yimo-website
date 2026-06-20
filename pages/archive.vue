ffbe416751cef4c45104210fd8e8ae37ed13cfc0
welu2027 <wenhaolu2027@gmail.com>
2026-06-20 01:19:45 -0400

    Update archive.vue

--- pages/archive.vue
+++ pages/archive.vue
@@ -6,16 +6,10 @@
     <section>
       <h2>YIMO 1</h2>
       <ul>
-        <li><strong>YIMO 1 Written Round Problems:</strong> <a href="/solutions/yimo-1-written-round-problems.pdf" target="_blank" rel="noopener">Download →</a></li>
-        <li><strong>YIMO 1 Written Round Solutions:</strong> <a href="/solutions/yimo-1-written-round-solutions.pdf" target="_blank" rel="noopener">Download →</a></li>
-        <li><strong>YIMO 1 Final Round Problems:</strong> <a href="/solutions/yimo-1-final-round-problems.pdf" target="_blank" rel="noopener">Download →</a></li>
-        <li><strong>YIMO 1 Final Round Solutions:</strong> <a href="/solutions/yimo-1-final-round-solutions.pdf" target="_blank" rel="noopener">Download →</a></li>
-      </ul>
-    </section>
-
-    <section>
-      <h2>Slideshows</h2>
-      <ul>
+        <li><strong>Written Round Problems:</strong> <a href="/solutions/yimo-1-written-round-problems.pdf" target="_blank" rel="noopener">View →</a></li>
+        <li><strong>Written Round Solutions:</strong> <a href="/solutions/yimo-1-written-round-solutions.pdf" target="_blank" rel="noopener">View →</a></li>
+        <li><strong>Final Round Problems:</strong> <a href="/solutions/yimo-1-final-round-problems.pdf" target="_blank" rel="noopener">View →</a></li>
+        <li><strong>Final Round Solutions:</strong> <a href="/solutions/yimo-1-final-round-solutions.pdf" target="_blank" rel="noopener">View →</a></li>
         <li><strong>Estimathon Workshop:</strong> <a href="https://drive.google.com/file/d/1oh6rpw89WBEReqoPNOspdsYvr8wfMhRN/view?usp=sharing" target="_blank" rel="noopener">View →</a></li>
         <li><strong>Window A Awards Ceremony:</strong> <a href="https://drive.google.com/file/d/1G-yiurPMI482UgBuHrC6nesXBHA9H1Vv/view?usp=sharing" target="_blank" rel="noopener">View →</a></li>
         <li><strong>Window B Awards Ceremony:</strong> <a href="https://drive.google.com/file/d/1U8lIAZQBRyUb5RTtIY40Q1zzih_q6iUM/view?usp=sharing" target="_blank" rel="noopener">View →</a></li>
